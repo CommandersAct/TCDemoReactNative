a4457d4a3792de1ed58bddd5638d706883ecc6cc
Abdelhakim SAID <abdelhakim.said@commandersact.com>
2023-10-30 15:44:08 +0100

    added ios & new singletons

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
-import { StyleSheet, SafeAreaView, TouchableOpacity, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, TouchableOpacity, Text, View } from 'react-native';
 import * as TCServerSide from 'tcserverside-react-native';
-import {TCDevice, TCApp} from 'tcserverside-react-native';
-import {TCBeginCheckoutEvent} from 'tcserverside-react-native';
-import {TCUser} from "tccore-react-native";
+import {TCAppInstance, TCDeviceInstance, TCBeginCheckoutEvent, TCItem, TCProduct} from 'tcserverside-react-native';
+import {TCUserInstance} from "tccore-react-native";
 import * as TCConsent from 'tcconsent-react-native';
 
-function test()
-{
-  console.log(TCUser.getInstance().anonymous_id);
-  let t = new TCBeginCheckoutEvent()
-  t.name = "haim"
-}
+let mockConsent = new Map([
+  ['PRIVACY_CAT_1', '1'],
+  ['PRIVACY_CAT_2', '1'],
+  ['PRIVACY_CAT_3', '0'],
+  ['PRIVACY_VEN_1', '0'],
+  ['PRIVACY_CAT_2', '1'],
+]);
 
 export default function App() {
+  inialise()
   return (
     <View style={styles.appContainer}>
       <ButtonRow />
@@ -21,97 +22,165 @@ export default function App() {
   );
 }
 
-async function initialiseTCConsent()
+async function inialise()
 {
-  await TCConsent.setSiteIDPrivacyID(3311, 2929)
+  TCConsent.setSiteIDPrivacyID(3311, 2929)
+  TCServerSide.initServerSide(3311, 'a_source_key')
 }
 
 function printTCUser()
 {
-  console.log(TCUser.getInstance().anonymous_id)
+  console.log(TCUserInstance.anonymous_id)
 }
 
 const ButtonRow = () => {
 
   return (
-  <View style={styles.container}>
-    <TouchableOpacity
-      style={styles.consentButton}
-      onPress={initialiseTCConsent}>
-      <Text style={styles.buttonText}>Re-init TCConsent</Text>
-    </TouchableOpacity>
+    <ScrollView>
+       <View style={styles.container}>
 
-    <TouchableOpacity
-      style={styles.consentButton}
-      onPress={printTCUser}>
-      <Text style={styles.buttonText}>Print TCUser anonymous_id</Text>
-    </TouchableOpacity>
+       <TouchableOpacity
+          style={styles.button}
+          onPress={executeEvent}>
+          <Text style={styles.buttonText}>Send Event</Text>
+        </TouchableOpacity>
 
-    <TouchableOpacity
-      style={styles.consentButton}
-      onPress={TCConsent.acceptAllConsent}>
-      <Text style={styles.buttonText}>acceptAllConsent</Text>
-    </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={printValues}>
+          <Text style={styles.buttonText}>Print values</Text>
+        </TouchableOpacity>
 
-    <TouchableOpacity
-      style={styles.consentButton}
-      onPress={TCConsent.refuseAllConsent}>
-      <Text style={styles.buttonText}>refuseAllConsent</Text>
-    </TouchableOpacity>
 
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => TCServerSide.initServerSide(3311, 'a_source_key')}>
-      <Text style={styles.buttonText}>Init ServerSide</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={executeEvent}>
-      <Text style={styles.buttonText}>Send Event</Text>
-    </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.acceptAllConsent}>
+          <Text style={styles.buttonText}>acceptAllConsent</Text>
+        </TouchableOpacity>
 
-    <TouchableOpacity
-      style={styles.button}
-      onPress={printValues}>
-      <Text style={styles.buttonText}>Print values</Text>
-    </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.refuseAllConsent}>
+          <Text style={styles.buttonText}>refuseAllConsent</Text>
+        </TouchableOpacity>
 
-    <TouchableOpacity
-      style={styles.button}
-      onPress={setAdditionalValues}>
-      <Text style={styles.buttonText}>Add Additional properties</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={TCServerSide.disableServerSide}>
-      <Text style={styles.buttonText}>Disable ServerSide</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={TCServerSide.enableServerSide}>
-      <Text style={styles.buttonText}>Enable ServerSide</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={TCServerSide.addAdvertisingID}>
-      <Text style={styles.buttonText}>addAdvertisingID</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={TCServerSide.enableRunningInBackground}>
-      <Text style={styles.buttonText}>enableRunningInBackground</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={removeAdditionalValues}>
-      <Text style={styles.buttonText}>Remove Addional Value</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={clearAdditionalProperties}>
-      <Text style={styles.buttonText}>Clear Addional Value</Text>
-    </TouchableOpacity>
-  </View>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.showPrivacyCenter}>
+          <Text style={styles.buttonText}>showPrivacyCenter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={() => TCConsent.useACString(true)}>
+          <Text style={styles.buttonText}>useACString</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={() => TCConsent.customPCMSetSiteID(121,121)}>
+          <Text style={styles.buttonText}>Init with custom PC</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={() => TCConsent.setConsentDuration(9)}>
+          <Text style={styles.buttonText}>setConsentDuration</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.useCustomPublisherRestrictions}>
+          <Text style={styles.buttonText}>useCustomPublisherRestrictions</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={() => TCConsent.saveConsentFromPopUp(mockConsent)}>
+          <Text style={styles.buttonText}>saveConsentFromPopUp</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={() => TCConsent.saveConsent(mockConsent)}>
+          <Text style={styles.buttonText}>saveConsent</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={() => TCConsent.saveConsentFromConsentSourceWithPrivacyAction(mockConsent, TCConsent.ETCConsentSource.POP_UP, TCConsent.ETCConsentAction.SAVE)}>
+          <Text style={styles.buttonText}>saveConsentFromConsentSourceWithPrivacyAction</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.statEnterPCToVendorScreen}>
+          <Text style={styles.buttonText}>statEnterPCToVendorScreen</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.statShowVendorScreen}>
+          <Text style={styles.buttonText}>statShowVendorScreen</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.statViewPrivacyPoliciesFromPrivacyCenter}>
+          <Text style={styles.buttonText}>statViewPrivacyPoliciesFromPrivacyCenter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.statViewPrivacyCenter}>
+          <Text style={styles.buttonText}>statViewPrivacyCenter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.statViewBanner}>
+          <Text style={styles.buttonText}>statViewBanner</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={TCConsent.resetSavedConsent}>
+          <Text style={styles.buttonText}>resetSavedConsent</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.consentButton}
+          onPress={() => TCConsent.setLanguage("fr")}>
+          <Text style={styles.buttonText}>setLanguage to "fr"</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={setAdditionalValues}>
+          <Text style={styles.buttonText}>Add Additional properties</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={TCServerSide.disableServerSide}>
+          <Text style={styles.buttonText}>Disable ServerSide</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={TCServerSide.enableServerSide}>
+          <Text style={styles.buttonText}>Enable ServerSide</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={TCServerSide.addAdvertisingID}>
+          <Text style={styles.buttonText}>addAdvertisingID</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={TCServerSide.enableRunningInBackground}>
+          <Text style={styles.buttonText}>enableRunningInBackground</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={removeAdditionalValues}>
+          <Text style={styles.buttonText}>Remove Addional Value</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={clearAdditionalProperties}>
+          <Text style={styles.buttonText}>Clear Addional Value</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
 );
 };
 
@@ -121,6 +190,49 @@ function executeEvent()
   event.currency = "USD"
   event.value = 12
 
+  let product_1 = new TCProduct()
+
+  product_1.ID = "product_1_id";
+  product_1.name = "product_1_name";
+  product_1.currency = "product_1_currency";
+  product_1.categories = ["prod_1_cat1", "prod_1_cat2"];
+  product_1.brand = "prod_1_brand"
+  product_1.colors = ["prod_1_col1", "prod_1_col2"]
+  product_1.size = "prod_1_size";
+  let item_1 = new TCItem();
+  item_1.ID = "item_1_id"
+  item_1.variant = "item_1_variant"
+  item_1.list_position = 1
+  item_1.discount = 0.21
+  item_1.quantity = 3
+  item_1.affiliation = "item_1_affiliation"
+  item_1.coupon = "item_1_coupon"
+  item_1.product = product_1
+
+  let product_2 = new TCProduct()
+
+  product_2.ID = "product_2_id";
+  product_2.name = "product_2_name";
+  product_2.currency = "product_2_currency";
+  product_2.categories = ["prod_2_cat1", "prod_2_cat2"];
+  product_2.brand = "prod_2_brand"
+  product_2.colors = ["prod_2_col1", "prod_2_col2"]
+  product_2.size = "prod_2_size";
+  let item_2 = new TCItem();
+  item_2.ID = "item_2_id"
+  item_2.variant = "item_2_variant"
+  item_2.list_position = 1
+  item_2.discount = 0.21
+  item_2.quantity = 3
+  item_2.affiliation = "item_2_affiliation"
+  item_2.coupon = "item_2_coupon"
+  item_2.product = product_2
+
+  event.items = [item_1, item_2]
+
+  event.addAdditionalProperty("add_property", "addValue")
+
+  console.log(event)
   TCServerSide.execute(event)
 }
 
@@ -129,91 +241,85 @@ function printValues()
 
   console.log("TCAPP values")
 
-  console.log(TCApp.getInstance().name)
-  console.log(TCApp.getInstance().version)
-  console.log(TCApp.getInstance().build)
-  console.log(TCApp.getInstance().nameSpace)
-  console.log(TCApp.getInstance().coreVersion)
-  console.log(TCApp.getInstance().serverSideVersion)
+  console.log(TCAppInstance.name)
+  console.log(TCAppInstance.version)
+  console.log(TCAppInstance.build)
+  console.log(TCAppInstance.nameSpace)
+  console.log(TCAppInstance.coreVersion)
+  console.log(TCAppInstance.serverSideVersion)
 
 
   console.log("TCDevice values")
 
-  console.log(TCDevice.getInstance().sdkID)
-  console.log(TCDevice.getInstance().manufacturer)
-  console.log(TCDevice.getInstance().model)
-  console.log(TCDevice.getInstance().name)
-  console.log(TCDevice.getInstance().type)
-  console.log(TCDevice.getInstance().timezone)
-  console.log(TCDevice.getInstance().osName)
-  console.log(TCDevice.getInstance().osVersion)
-  console.log(TCDevice.getInstance().screenWidth)
-  console.log(TCDevice.getInstance().screenHeight)
+  console.log(TCDeviceInstance.sdkID)
+  console.log(TCDeviceInstance.manufacturer)
+  console.log(TCDeviceInstance.model)
+  console.log(TCDeviceInstance.name)
+  console.log(TCDeviceInstance.type)
+  console.log(TCDeviceInstance.timezone)
+  console.log(TCDeviceInstance.osName)
+  console.log(TCDeviceInstance.osVersion)
+  console.log(TCDeviceInstance.screenWidth)
+  console.log(TCDeviceInstance.screenHeight)
 
   console.log("TCUser values")
 
-  console.log(TCUser.getInstance().consentID)
-  console.log(TCUser.getInstance().anonymous_id)
+  console.log(TCUserInstance.consentID)
+  console.log(TCUserInstance.anonymous_id)
 
 }
 
 function setAdditionalValues()
 {
-  TCDevice.getInstance().addAdditionalProperty("additional_string", "additional_val_string")
-  TCDevice.getInstance().addAdditionalPropertyWithNumberValue("additional_num", 31.03)
-  TCDevice.getInstance().addAdditionalPropertyWithNumberValue("additional_int", 31)
-  TCDevice.getInstance().addAdditionalPropertyWithBooleanValue("additional_bool", true)
-  TCDevice.getInstance().addAdditionalPropertyWithArrayValue("additional_list_int",[31,1998,30])
-  TCDevice.getInstance().addAdditionalPropertyWithArrayValue("additional_list_string",["SAID","Abdelhakim"])
+  TCDeviceInstance.addAdditionalProperty("additional_string", "additional_val_string")
+  TCDeviceInstance.addAdditionalPropertyWithNumberValue("additional_num", 31.03)
+  TCDeviceInstance.addAdditionalPropertyWithNumberValue("additional_int", 31)
+  TCDeviceInstance.addAdditionalPropertyWithBooleanValue("additional_bool", true)
 
-  TCApp.getInstance().addAdditionalProperty("additional_string", "additional_val_string")
-  TCApp.getInstance().addAdditionalPropertyWithNumberValue("additional_num", 31.03)
-  TCApp.getInstance().addAdditionalPropertyWithNumberValue("additional_int", 31)
-  TCApp.getInstance().addAdditionalPropertyWithBooleanValue("additional_bool", true)
-  TCApp.getInstance().addAdditionalPropertyWithArrayValue("additional_list_int",[31,1998,30])
-  TCApp.getInstance().addAdditionalPropertyWithArrayValue("additional_list_string",["SAID","Abdelhakim"])
+  TCAppInstance.addAdditionalProperty("additional_string", "additional_val_string")
+  TCAppInstance.addAdditionalPropertyWithNumberValue("additional_num", 31.03)
+  TCAppInstance.addAdditionalPropertyWithNumberValue("additional_int", 31)
+  TCAppInstance.addAdditionalPropertyWithBooleanValue("additional_bool", true)
 
-  TCUser.getInstance().addAdditionalProperty("additional_string", "additional_val_string")
-  TCUser.getInstance().addAdditionalPropertyWithNumberValue("additional_num", 31.03)
-  TCUser.getInstance().addAdditionalPropertyWithNumberValue("additional_int", 31)
-  TCUser.getInstance().addAdditionalPropertyWithBooleanValue("additional_bool", true)
-  TCUser.getInstance().addAdditionalPropertyWithArrayValue("additional_list_int",[31,1998,30])
-  TCUser.getInstance().addAdditionalPropertyWithArrayValue("additional_list_string",["SAID","Abdelhakim"])
+  TCUserInstance.addAdditionalProperty("additional_string", "additional_val_string")
+  TCUserInstance.addAdditionalPropertyWithNumberValue("additional_num", 31.03)
+  TCUserInstance.addAdditionalPropertyWithNumberValue("additional_int", 31)
+  TCUserInstance.addAdditionalPropertyWithBooleanValue("additional_bool", true)
 
   console.log("successfully setted")
 }
 
 function clearAdditionalProperties()
 {
-  TCDevice.getInstance().clearAdditionalProperties()
-  TCApp.getInstance().clearAdditionalProperties()
-  TCUser.getInstance().clearAdditionalProperties()
+  TCDeviceInstance.clearAdditionalProperties()
+  TCAppInstance.clearAdditionalProperties()
+  TCUserInstance.clearAdditionalProperties()
 }
 
 function removeAdditionalValues()
 {
-  TCDevice.getInstance().removeAdditionalProperty("additional_string")
-  TCDevice.getInstance().removeAdditionalProperty("additional_num")
-  TCDevice.getInstance().removeAdditionalProperty("additional_int")
-  TCDevice.getInstance().removeAdditionalProperty("additional_bool")
-  TCDevice.getInstance().removeAdditionalProperty("additional_list_int")
-  TCDevice.getInstance().removeAdditionalProperty("additional_list_string")
-  TCDevice.getInstance().removeAdditionalProperty("additional_map")
+  TCDeviceInstance.removeAdditionalProperty("additional_string")
+  TCDeviceInstance.removeAdditionalProperty("additional_num")
+  TCDeviceInstance.removeAdditionalProperty("additional_int")
+  TCDeviceInstance.removeAdditionalProperty("additional_bool")
+  TCDeviceInstance.removeAdditionalProperty("additional_list_int")
+  TCDeviceInstance.removeAdditionalProperty("additional_list_string")
+  TCDeviceInstance.removeAdditionalProperty("additional_map")
 
-  TCApp.getInstance().removeAdditionalProperty("additional_string")
-  TCApp.getInstance().removeAdditionalProperty("additional_num")
-  TCApp.getInstance().removeAdditionalProperty("additional_int")
-  TCApp.getInstance().removeAdditionalProperty("additional_bool")
-  TCApp.getInstance().removeAdditionalProperty("additional_list_int")
-  TCApp.getInstance().removeAdditionalProperty("additional_list_string")
+  TCAppInstance.removeAdditionalProperty("additional_string")
+  TCAppInstance.removeAdditionalProperty("additional_num")
+  TCAppInstance.removeAdditionalProperty("additional_int")
+  TCAppInstance.removeAdditionalProperty("additional_bool")
+  TCAppInstance.removeAdditionalProperty("additional_list_int")
+  TCAppInstance.removeAdditionalProperty("additional_list_string")
 
 
-  TCUser.getInstance().removeAdditionalProperty("additional_string")
-  TCUser.getInstance().removeAdditionalProperty("additional_num")
-  TCUser.getInstance().removeAdditionalProperty("additional_int")
-  TCUser.getInstance().removeAdditionalProperty("additional_bool")
-  TCUser.getInstance().removeAdditionalProperty("additional_list_int")
-  TCUser.getInstance().removeAdditionalProperty("additional_list_string")
+  TCUserInstance.removeAdditionalProperty("additional_string")
+  TCUserInstance.removeAdditionalProperty("additional_num")
+  TCUserInstance.removeAdditionalProperty("additional_int")
+  TCUserInstance.removeAdditionalProperty("additional_bool")
+  TCUserInstance.removeAdditionalProperty("additional_list_int")
+  TCUserInstance.removeAdditionalProperty("additional_list_string")
 }
 
 const styles = StyleSheet.create({
